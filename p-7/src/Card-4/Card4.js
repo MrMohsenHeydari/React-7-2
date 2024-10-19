b7e60b0ad67ccced2e6f21f9252c721cdaf1016f
MrMohsenHeydari <mohsen.heydari061@gmail.com>
2024-10-19 00:56:01 -0700

    Create Card4.js

---
+++ p-7/src/Card-4/Card4.js
@@ -0,0 +1,26 @@
+import "./Card4.css"
+
+const Card4 = () => {
+    return (
+        <>
+            <div className="card-4">
+                <div className="pro">
+                    <img src="https://dkstatics-public.digikala.com/digikala-products/aa62c502c79074150ba4263e737f5170438fb2a9_1644329857.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90" alt="" />
+                    <div className="pro-2">
+                        <h4>دسته بازی</h4>
+                        <p>دسته بازی مدل doallshock 4، یک ابزار کنترلی پیشرفته است که طراحی شده تا تجربه گیمینگ را به سطح جدیدی ارتقا دهد. این دسته بازی با ترکیب ویژگی‌های نوآورانه و کاربردی خود، گیمرها را قادر می‌سازد تا با دقت، سرعت و کنترل بیشتری در بازی‌های مورد علاقه خود شرکت کنند.</p>
+                    </div>
+                </div>
+                <div className="pro">
+                    <img src="https://dkstatics-public.digikala.com/digikala-products/aa62c502c79074150ba4263e737f5170438fb2a9_1644329857.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90" alt="" />
+                    <div className="pro-2">
+                        <h4>دسته بازی</h4>
+                        <p>دسته بازی مدل doallshock 4، یک ابزار کنترلی پیشرفته است که طراحی شده تا تجربه گیمینگ را به سطح جدیدی ارتقا دهد. این دسته بازی با ترکیب ویژگی‌های نوآورانه و کاربردی خود، گیمرها را قادر می‌سازد تا با دقت، سرعت و کنترل بیشتری در بازی‌های مورد علاقه خود شرکت کنند.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card4;
